9dde41137c195bbc8614e3234e6eb14fee514ed7
Christian von Elm <christian.von_elm@tu-dresden.de>
2023-07-26 13:03:37 +0200

    Add python script to generate c struct from tracepoint format

---
+++ tp_to_struct.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#Generates a C struct from the /sys/kernel/debug/tracing/events/*/*/format file
+#Needs access to debugfs obviously
+
+import re, sys
+
+
+def size_to_type(size, signed):
+    if(signed == 0):
+        if(size == 1):
+            return "uint8_t";
+        if(size == 2):
+            return "uint16_t";
+        if(size == 4):
+            return "uint32_t";
+        if(size == 8):
+            return "uint64_t";
+        return "uint8_t[{}]".format(size)
+    if(signed == 1):
+        if(size == 1):
+            return "int8_t";
+        if(size == 2):
+            return "int16_t";
+        if(size == 4):
+            return "int32_t";
+        if(size == 8):
+            return "int64_t";
+    sys.exit("Unknown size: " + size)
+
+if len(sys.argv) != 3:
+    sys.exit("Too few arguments specified")
+
+group = sys.argv[1]
+tracepoint = sys.argv[2]
+
+
+pattern = re.compile('\s+field:.*\s([a-zA-Z_]+).*;\s+offset:(\d+).*size:(\d+).*signed:(\d);')
+
+f = open("/sys/kernel/debug/tracing/events/{}/{}/format".format(group, tracepoint));
+
+print("struct __attribute((__packed__)) {}_{}".format(group, tracepoint))
+print("{")
+
+cur_offset = 0
+padding_num = 0;
+for line in f:
+    match = pattern.match(line)
+    if match:
+        name = match.group(1)
+        next_offset = int(match.group(2))
+        size = int(match.group(3))
+        signed = int(match.group(4))
+
+        if (next_offset > cur_offset):
+            print("    char padding{} [{}];".format(padding_num, next_offset-cur_offset))
+            cur_offset = next_offset
+            padding_num = padding_num + 1
+        if('[' in line):
+            print("    char {}[{}];".format(name, size))
+        else:
+            print("    {} {};".format(size_to_type(size, signed), name))
+        cur_offset = cur_offset + size;
+print("};")
